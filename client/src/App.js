ac1715503f5ccc5fcb64518aca7cdba41e1d963b
leehi9817 <leehi9817@gmail.com>
2025-03-11 16:57:46 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Customer from "./components/Customer";
 import "./App.css";
 import Paper from "@mui/material/Paper";
@@ -7,54 +7,40 @@ import TableHead from "@mui/material/TableHead";
 import TableBody from "@mui/material/TableBody";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { makeStyles } from "@mui/styles";
+import { withStyles } from "@mui/styles";
 
-const theme = createTheme();
-
-const useStyles = makeStyles(() => ({
+const styles = () => ({
   root: {
     width: "100%",
-    marginTop: theme.spacing(3),
+    marginTop: "18px",
     overflowX: "auto",
   },
   table: {
     minWidth: 1080,
   },
-}));
+});
 
-const customers = [
-  {
-    id: 1,
-    image: "https://picsum.photos/64?random=1",
-    name: "이혜인",
-    birthday: "980820",
-    gender: "여자",
-    job: "직장인",
-  },
-  {
-    id: 2,
-    image: "https://picsum.photos/64?random=2",
-    name: "이혜원",
-    birthday: "020509",
-    gender: "여자",
-    job: "대학생",
-  },
-  {
-    id: 3,
-    image: "https://picsum.photos/64?random=3",
-    name: "이해찬",
-    birthday: "100613",
-    gender: "남자",
-    job: "중학생",
-  },
-];
+class App extends Component {
+  state = {
+    customers: [],
+  };
 
-function App() {
-  const classes = useStyles();
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }
 
-  return (
-    <ThemeProvider theme={theme}>
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
@@ -68,24 +54,26 @@ function App() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map((c) => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            })}
+            {this.state.customers
+              ? this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              : []}
           </TableBody>
         </Table>
       </Paper>
-    </ThemeProvider>
-  );
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://picsum.photos/64?random=1",
+      name: "이혜인",
+      birthday: "980820",
+      gender: "여자",
+      job: "직장인",
+    },
+    {
+      id: 2,
+      image: "https://picsum.photos/64?random=2",
+      name: "이혜원",
+      birthday: "020509",
+      gender: "여자",
+      job: "대학생",
+    },
+    {
+      id: 3,
+      image: "https://picsum.photos/64?random=3",
+      name: "이해찬",
+      birthday: "100613",
+      gender: "남자",
+      job: "중학생",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
